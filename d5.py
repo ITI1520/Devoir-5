c8c41767dc28b6d9d5169e391570ad8a53f2388a
SamuelDicaire <149833122+SamuelDicaire@users.noreply.github.com>
2023-11-23 20:48:59 +0000

    Fini les question 1,2,3

--- d5.py
+++ d5.py
@@ -3,41 +3,92 @@
 print(" ")
 print("Question 1")
 print(" ")
+# Question 1
 
 def triangle(x):
-    # int -> str 
-    """ Fonction récursif qui prend un entier et retourne une piramide aligné à gauche d'une longueur dépendente du nombre donné et
-        utilise le symbole * pour le faire """
+    # int -> str
+    """ Fonction récursive qui prend un entier et retourne un dessin composé d'étoiles sous forme de triangle """
     
     if x <= 0:
-         return None
+        return ""
+    
     else:
-        triangle(x - 1)  
-        print('*' * x)
+        pyramide = triangle(x - 1) + '*' * x + '\n'
         
-# Exemple
-triangle(5)
+        return pyramide
 
-# Question 2
+# Exemple question 1
+print(triangle(5))
 
 print(" ")
 print("Question 2")
 print(" ")
 
-def etoiles(n):
+# Question 2
+
+def etoiles(x):
     # int -> str
     """ Fonction récursif qui prend un entier et retourne une piramide inversé aligné a gauche et ensuite une piramide inversé a gauche
         d'une longeur dépendante du nombre donné et utilise le symbole * pour le faire """
     
-    if n <= 0:
-        return None
+    if x <= 0:
+        return ""
+    
     else:
-       
-        print('*' * n)
+        pyramide = '*' * x + '\n'
 
-        etoiles(n - 1)
+        dessin_f = pyramide + etoiles(x - 1) + pyramide
+        
+        return dessin_f
+      
 
-        print('*' * n)
+# Exemple question 2
+print(etoiles(4))
 
-# Exemple
-etoiles(4)
+print(" ")
+print("Question 3")
+print(" ")
+
+# Question 3
+
+def prodListePos_rec(liste, long):
+    # List, len -> int
+    """ Fonction qui prend une liste un la longueur de la liste et qui retourne le produit de tout les nombre >0 dans la liste """
+    
+    if long == 0:
+        return 1
+    
+    elif liste[long - 1] > 0:
+        p1 = liste[long - 1] * prodListePos_rec(liste, long - 1)
+        
+        return p1
+    
+    else:
+        p2 = prodListePos_rec(liste, long - 1)
+        
+        return p2
+
+def prodLRec1(liste):
+    
+    if len(liste) == 0:
+        return 1
+    
+    elif liste[0] > 0:
+        p1 = liste[0] * prodLRec1(liste[1:])
+        
+        return p1
+    
+    else :
+        p2 = prodLRec1(liste[1:])
+        
+        return p2
+
+# Exemples prodListePos_rec
+l = [1, -2, 5, 0, 6, -5]
+print(prodListePos_rec(l, len(l)))  
+print(prodLRec1(l))                 
+
+# Exemple prodListePos_rec
+l = [1, -2, 5, 0, 6, -5]
+print(prodListePos_rec(l, len(l)))  
+print(prodLRec1(l))                 
